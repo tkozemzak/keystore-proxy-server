6cde5c2b92b6411248e83b743b27fab2451a094a
Tim Kozemzak <timkozemzak@gmail.com>
2021-02-23 20:23:04 -0700

    fix endpoints and data return for browser-homepage project

--- src/api/browser-homepage.js
+++ src/api/browser-homepage.js
@@ -7,19 +7,23 @@ const MAP_URL = "https://maps.googleapis.com/maps/api/geocode/json?latlng=";
 const WEATHER_URL = `https://api.openweathermap.org/data/2.5/weather?q=`;
 
 router.get("/geomap/:lat/:long", async (req, res, next) => {
-  //
-
   const { data } = await axios.get(
     `${MAP_URL}${req.params.lat},${req.params.long}&sensor=true&key=${process.env.GOOGLE_GEOCODE_API_KEY}`
   );
-  res.json(data);
+  res.json(data.results[0].address_components[3].short_name);
+  //   console.log("city", data);
+  //   res.json(data);
 });
 
-router.get("/weather/:city", async (req, res, next) => {
-  const { data } = await axios.get(
-    `${WEATHER_URL}${req.params.city},us&appid=${process.env.WEATHER_API_KEY}&units=imperial`
-  );
-  res.json(data);
-});
+try {
+  router.get("/weather/:city", async (req, res, next) => {
+    const { data } = await axios.get(
+      `${WEATHER_URL}${req.params.city},us&appid=${process.env.WEATHER_API_KEY}&units=imperial`
+    );
+    res.json(data);
+  });
+} catch (err) {
+  res.send(err);
+}
 
 module.exports = router;
