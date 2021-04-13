26e3a33f1da4a91df695fba411204cea6144856f
tkozemzak <timkozemzak@gmail.com>
2021-04-12 20:14:06 -0700

    add FS and HTTPS

--- src/index.js
+++ src/index.js
@@ -1,6 +1,17 @@
-const app = require('./app');
+const app = require("./app");
+const https = require("https");
+const fs = require("fs");
 
 const port = process.env.PORT || 8000;
-app.listen(port, () => {
-  console.log(`Listening: http://localhost:${port}`);
-});
+
+https
+  .createServer(
+    {
+      key: fs.readFileSync("server.key"),
+      cert: fs.readFileSync("server.cert"),
+    },
+    app
+  )
+  .listen(process.env.PORT || 8000, () => {
+    console.log(`Listening: http://localhost:${port}`);
+  });
