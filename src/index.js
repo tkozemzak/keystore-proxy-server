420e05ec6c7f457160d26bad2fdeda1b97ec35d3
tkozemzak <timkozemzak@gmail.com>
2021-05-16 21:23:42 -0700

    remove https

--- src/index.js
+++ src/index.js
@@ -4,14 +4,14 @@ const fs = require("fs");
 
 const port = process.env.PORT || 8000;
 
-https
-  .createServer(
-    {
-      key: fs.readFileSync("server.key"),
-      cert: fs.readFileSync("server.cert"),
-    },
-    app
-  )
-  .listen(process.env.PORT || 8000, () => {
-    console.log(`Listening: http://localhost:${port}`);
-  });
+// https
+//   .createServer(
+//     {
+//       key: fs.readFileSync("server.key"),
+//       cert: fs.readFileSync("server.cert"),
+//     },
+//     app
+//   )
+app.listen(process.env.PORT || 8000, () => {
+  console.log(`Listening: http://localhost:${port}`);
+});
