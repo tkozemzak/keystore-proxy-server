24e9a27e3eb21a882d239cb6c0faf769456ca27b
Ubuntu <ubuntu@ip-172-31-6-16.us-west-1.compute.internal>
2021-05-20 23:13:09 +0000

    from ec2

--- src/index.js
+++ src/index.js
@@ -1,17 +1,20 @@
 const app = require("./app");
 const https = require("https");
 const fs = require("fs");
+const cors = require('cors')
 
 const port = process.env.PORT || 8000;
 
-https
-  .createServer(
-    {
-      key: fs.readFileSync("server.key"),
-      cert: fs.readFileSync("server.cert"),
-    },
-    app
-  )
-  .listen(process.env.PORT || 8000, () => {
+app.use(cors());
+
+//https
+ // .createServer(
+ //   {
+ //     key: fs.readFileSync("server.key"),
+ //     cert: fs.readFileSync("server.cert"),
+  //  },
+ //   app
+ // )
+ app.listen(process.env.PORT || 8000, () => {
     console.log(`Listening: http://localhost:${port}`);
   });
