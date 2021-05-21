6cf203bc37296f546f05866fc455c0d94a21fd75
tkozemzak <timkozemzak@gmail.com>
2021-05-20 17:22:03 -0700

    fix merge issue

--- src/index.js
+++ src/index.js
@@ -5,21 +5,9 @@ const cors = require('cors')
 
 const port = process.env.PORT || 8000;
 
-<<<<<<< HEAD
 app.use(cors());
 
-//https
- // .createServer(
- //   {
- //     key: fs.readFileSync("server.key"),
- //     cert: fs.readFileSync("server.cert"),
-  //  },
- //   app
- // )
- app.listen(process.env.PORT || 8000, () => {
-    console.log(`Listening: http://localhost:${port}`);
-  });
-=======
+
 // https
 //   .createServer(
 //     {
@@ -28,7 +16,6 @@ app.use(cors());
 //     },
 //     app
 //   )
-app.listen(process.env.PORT || 8000, () => {
+app.listen(port, () => {
   console.log(`Listening: http://localhost:${port}`);
 });
->>>>>>> 420e05ec6c7f457160d26bad2fdeda1b97ec35d3
